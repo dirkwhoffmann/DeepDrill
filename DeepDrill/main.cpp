431a409b07634bbb23629176b6419b77f0ad631b
Dirk Hoffmann <dirk.hoffmann@me.com>
2021-07-06 14:31:06 +0200

    Initial Commit

---
+++ DeepDrill/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DeepDrill
+//
+//  Created by Dirk Hoffmann on 06.07.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
