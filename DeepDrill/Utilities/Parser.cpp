36cb807d799120a9363d9ef8981bb6a128e940ff
Dirk Hoffmann <dirk.hoffmann@me.com>
2021-07-07 08:14:57 +0200

    Regrouping

---
+++ DeepDrill/Utilities/Parser.cpp
@@ -0,0 +1,12 @@
+// -----------------------------------------------------------------------------
+// This file is part of DeepDrill
+//
+// A Mandelbrot generator based on perturbation and series approximation
+//
+// Copyright (C) Dirk W. Hoffmann. www.dirkwhoffmann.de
+// Licensed under the GNU General Public License v3
+//
+// See https://www.gnu.org for license information
+// -----------------------------------------------------------------------------
+
+#include "Parser.h"
---
+++ DeepDrill/Utilities/Parser.h
@@ -0,0 +1,24 @@
+// -----------------------------------------------------------------------------
+// This file is part of DeepDrill
+//
+// A Mandelbrot generator based on perturbation and series approximation
+//
+// Copyright (C) Dirk W. Hoffmann. www.dirkwhoffmann.de
+// Licensed under the GNU General Public License v3
+//
+// See https://www.gnu.org for license information
+// -----------------------------------------------------------------------------
+
+#pragma once
+
+#include "config.h"
+#include "Types.h"
+
+namespace dd {
+
+class Parser {
+    
+//    static void parse();
+};
+
+}
