c04ea044f186c4e3e64606bdf01b57a04a4f1698
Dirk Hoffmann <dirk.hoffmann@me.com>
2021-07-13 13:01:32 +0200

    Added new Xcode target

---
+++ DDImage/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DDImage
+//
+//  Created by Dirk Hoffmann on 13.07.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
