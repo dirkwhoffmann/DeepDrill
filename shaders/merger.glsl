972313921722c2d83ed801d6b0a5524091ce029e
Dirk Hoffmann <dirk.hoffmann@me.com>
2023-05-24 16:31:01 +0200

    Work on shaders

--- shaders/merger.glsl
+++ shaders/merger.glsl
@@ -8,5 +8,6 @@ void main()
     vec4 pixel1 = texture2D(current, coord);
     vec4 pixel2 = texture2D(prev, coord);
     vec4 pixel3 = texture2D(prevprev, coord);
+    // gl_FragColor = gl_Color * mix(pixel1, mix(pixel2, pixel3, 0.3), 0.3);
     gl_FragColor = gl_Color * pixel1;
 }
