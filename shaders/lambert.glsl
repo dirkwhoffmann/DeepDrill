e420030be599ae868b135ffdc98a8be53f208c8a
Dirk Hoffmann <dirk.hoffmann@me.com>
2023-07-02 15:16:21 +0200

    Work on shader

--- shaders/lambert.glsl
+++ shaders/lambert.glsl
@@ -1,11 +1,11 @@
 // Sampler for the color index map
-uniform sampler2D index;
+uniform sampler2D index;        // DEPRECATED
 
 // Sampler for the iteration count map and the lognorm map
 uniform sampler2D iter;
 uniform sampler2D lognorm;
 
-uniform sampler2D arg;
+uniform sampler2D arg;          // DEPRECATED
 
 // Sampler for the texture and the overlay image
 uniform sampler2D texture;
@@ -15,12 +15,27 @@ uniform sampler2D overlay;
 uniform sampler2D palette;
 
 // Sampler for the normal map
-uniform sampler2D normal;
+uniform sampler2D normal;       // DEPRECATED
 uniform sampler2D normalRe;
 uniform sampler2D normalIm;
 
 // Light direction
 uniform vec3 lightDir;
+
+// Palette scaling factor
+// uniform float paletteStretch;
+
+// Palette offset
+// uniform float paletteShift;
+
+// Texture image opacity
+// uniform float opacity;
+
+// Texture scaling factor
+// uniform float textureStretch;
+
+// Palette offset
+// uniform float textureShift;
 
 // See
 // https://stackoverflow.com/questions/15095909/from-rgb-to-hsv-in-opengl-glsl
@@ -96,15 +111,6 @@ vec3 compute_tex_pixel(vec2 coord)
     float PI = 3.141592653589793238;
     float sl = compute_sl(coord);
 
-    /*
-    vec4 normalMap = texture2D(normal, coord);
-    float re = (normalMap.g * 256.0 + normalMap.r); // / (256.0 * 255.0);
-    float im = (normalMap.a * 256.0 + normalMap.b); //  / (256.0 * 255.0);
-     */
-    // vec3 N = normalMap * 2.0 - 1.0;
-
-    // float arg = (atan(im, re) + PI) / (2.0 * PI);
-
     float nrmRe = decode_float(texture2D(normalRe, coord));
     float nrmIm = decode_float(texture2D(normalIm, coord));
 
@@ -115,7 +121,6 @@ vec3 compute_tex_pixel(vec2 coord)
     float py = mod(sl * 5.0, 1.0);
 
     return vec3(texture2D(texture, vec2(px,py)).xyz);
-//    return vec3(texture2D(texture, vec2(px,py)).xyz);
 }
 
 void main()
@@ -126,39 +131,39 @@ void main()
     // Adapted from https://github.com/mattdesl/lwjgl-basics/wiki/ShaderLesson6
     //
 
-    // RGBA of our diffuse color
-    vec4 diffuseColor =  vec4(deriveColor(coord), 1.0);
+    // Get diffuse color from palette image
+    float sl = mod(compute_sl(coord) / (2.0 * 3.14159), 1.0);
+    vec4 diffuseColor = texture2D(palette, vec2(sl, 0.0));
 
-    // RGB of our normal map
-    vec3 normalMap = texture2D(normal, coord).rgb;
+    // Mix with texture image
+    diffuseColor = mix(diffuseColor, vec4(compute_tex_pixel(coord), 1.0), 0.5);
 
-    // Normalize our vectors
-    vec3 N = normalize(normalMap * 2.0 - 1.0);
-    vec3 L = normalize(lightDir);
-
-    // Compute "N dot L" to determine our diffuse term
-    float lambert = max(dot(N, L), 0.0);
+    // Get normal vector and light vector
+    float nrmRe = decode_float(texture2D(normalRe, coord));
+    float nrmIm = decode_float(texture2D(normalIm, coord));
 
     // Modulate diffuse color
     vec3 final;
 
-    if (normalMap.x == 0.0 && normalMap.y == 0.0 && normalMap.z == 0.0) {
+    if (nrmRe == 0.0 && nrmIm == 0.0) {
 
         final = diffuseColor.rgb;
 
     } else {
 
-        float scale = 0.75;
+        vec3 N = normalize(vec3(nrmRe, nrmIm, 1.0));
+        vec3 L = normalize(lightDir);
+        float lambert = max(dot(N, L), 0.0);
+
+        float lambertScale = 0.75;
         vec3 hsv = rgb2hsv(diffuseColor.rgb);
-        hsv.z *= (lambert * scale) + 1.0 - 0.5 * scale;
+        hsv.z *= (lambert * lambertScale) + 1.0 - 0.5 * lambertScale;
         final = hsv2rgb(hsv);
     }
 
-    // Experimental
-    float sl = mod(compute_sl(coord) / (2.0 * 3.14159), 1.0);
-    final = texture2D(palette, vec2(sl, 0.0)).xyz;
-
-    final = mix(final, compute_tex_pixel(coord), 0.5);
+    // Superimpose the overlay image
+    vec4 ovl = texture2D(overlay, coord);
+    final = mix(final.xyz, ovl.xyz, ovl.a);
 
     gl_FragColor = gl_Color * vec4(final, 1.0);
 }
